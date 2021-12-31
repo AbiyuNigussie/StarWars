5ab6069edff9b63b55844d9b1e07e6a433d47415
AbiyuTolosa <abiyunigussie7@gmail.com>
2021-12-30 22:28:31 -0700

    finished result

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav>
+
             <Link to="/Planets"><button>PLANETS</button></Link>
             <Link to="/People"><button>PEOPLE</button></Link>
             <Link to="/Species"><button>SPECIES</button></Link>
